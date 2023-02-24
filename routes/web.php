9996de90012c5caf896c17460cd6d3d9238ec38c
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-02-24 10:34:16 -0300

    added crud system to app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CompanyCRUDController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('companies', CompanyCRUDController::class);
 });
+
+
+
 
 require __DIR__.'/auth.php';
